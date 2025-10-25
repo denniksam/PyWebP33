71383886dc0762a62e6ba60b2adbc06dd79455eb
dns-lector <157026463+dns-lector@users.noreply.github.com>
2025-10-25 16:19:57 +0300

    Templates

--- Project33/App33/views.py
+++ Project33/App33/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
 # технічно представлення - це функції, які приймають
 # запит (request) та формують відповідь (response)
 def hello(request) :
     return HttpResponse("Hello, world!")
+
+
+def home(request) :
+    template = loader.get_template('home.html')
+    context = {
+        'x': 10,
+        'y': 20,
+        'page_title': 'Домашня',
+        'page_header': 'Домашня сторінка'
+    }
+    return HttpResponse( template.render(context, request) )
+
